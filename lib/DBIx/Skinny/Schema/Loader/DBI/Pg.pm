a94d29a5cf0de6a59b839113549b9c14cd94c6bc
hatyuki <hatyuki29@gmail.com>
2010-10-30 13:47:57 +0900

    support PostgreSQL's schema
    TODO:
      てすとかきましょう

--- lib/DBIx/Skinny/Schema/Loader/DBI/Pg.pm
+++ lib/DBIx/Skinny/Schema/Loader/DBI/Pg.pm
@@ -5,13 +5,24 @@ use warnings;
 use base qw/DBIx::Skinny::Schema::Loader::DBI/;
 
 sub tables {
-    my $self = shift;
-    my $sth  = $self->{ dbh }->table_info('', 'public', undef, undef);
+    my $self   = shift;
+    my $schema = $self->{ schema } || 'public';
+    my $sth    = $self->{ dbh }->table_info('', $schema, undef, undef);
     my @tables;
     for my $rel (@{ $sth->fetchall_arrayref({}) }) {
         push @tables, $rel->{TABLE_NAME};
     }
     return \@tables;
+}
+
+sub table_columns {
+    my ($self, $table) = @_;
+    my $schema = $self->{ schema } || 'public';
+    my $sth    = $self->{ dbh }->prepare("select * from $schema.$table where 1 = 0");
+    $sth->execute;
+    my $retval = \@{$sth->{NAME_lc}};
+    $sth->finish;
+    return $retval;
 }
 
 1;
